713efdfbf0f8cb2c1ee36c35838f0b61a9eef264
thongchai <thongchai.sh@gmail.com>
2019-04-24 10:42:26 +0700

    tomcat initial

---
+++ bin/setenv.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/local/HRMS_POSTGRES/tools/jdk1.7.0_79
+
+JAVA_OPTS="-Djava.awt.headless=true -Dfile.encoding=UTF-8 -server -Xms1g -Xmx2g -XX:PermSize=512m -XX:MaxPermSize=2g -XX:+DisableExplicitGC -XX:+CMSClassUnloadingEnabled -Dorg.apache.el.parser.SKIP_IDENTIFIER_CHECK=true"
+
